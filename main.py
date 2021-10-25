8d7e223e0f7f81ec01479fe4b69db2478ad69c63
Lars Maier <lars.maier@tefax.net>
2021-10-25 20:43:52 +0200

    Send images as blocks and use quickchart.io.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/bin/env python3
+import base64
+import json
+import os.path
+import urllib.parse
 from datetime import datetime
 
 import pyArango.connection
 import argparse
 import urllib3
+import requests
 import matplotlib.pyplot as plt
 
 urllib3.disable_warnings()
@@ -25,6 +30,10 @@ def parse_arguments():
     parser.add_argument("--server.endpoint", default="http://localhost:8529/")
     parser.add_argument("--server.user", type=user_pass, default="root")
     parser.add_argument("--server.no-verify-tls", action='store_true', default=False)
+    parser.add_argument("--plot.output-dir", default=".")
+    parser.add_argument("--slack.auth-token")
+    parser.add_argument("--slack.channel-id")
+    parser.add_argument("--slack.web-hook")
     return parser.parse_args()
 
 
@@ -42,7 +51,7 @@ for doc in rlog
             filter doc.name == name
             sort doc.date
             limit 50
-            return {date: doc.date, values: doc.result})
+            return {date: doc.date, values: doc.result, config: doc.test})
             
     return {name, latest_results}"""
 
@@ -82,15 +91,99 @@ def plot(name, results, file=None):
     twin1.yaxis.label.set_color(p2.get_color())
     twin1.tick_params(axis='y', colors=p2.get_color())
     ax.legend(handles=plots)
-    plt.savefig(f"{name}.png")
+    plt.savefig(file)
+
+
+def to_chart_js(name, results):
+    plots = []
+    for d in ['min', 'p99', 'p99.9', 'avg']:
+        plots.append({
+            'label': d,
+            'yAxisID': 'ms',
+            'data': [1000 * r['values'][d] for r in results]
+        })
+    dates = [datetime.utcfromtimestamp(int(r['date'])).strftime('%Y-%m-%d') for r in results]
+    rps = [r['values']['rps'] for r in results]
+    chart = {
+        'type': 'line',
+        'data': {
+            'labels': dates,
+            'datasets': [*plots,
+                         {
+                             'label': 'rps',
+                             'yAxisID': 'rps',
+                             'data': rps
+                         }
+                         ],
+        },
+        'options': {
+            'title': {
+                'display': True,
+                'text': name,
+            },
+            'scales': {
+                'yAxes': [
+                    {
+                        'id': 'ms',
+                        'type': 'linear',
+                        'position': 'left',
+                        "ticks": {"suggestedMin": 0},
+                        'scaleLabel': {
+                            'display': True,
+                            'labelString': 'Time (ms)'
+                        }
+                    }, {
+                        'id': 'rps',
+                        'type': 'linear',
+                        'position': 'right',
+                        "ticks": {"suggestedMin": 0},
+                        'scaleLabel': {
+                            'display': True,
+                            'labelString': 'Requests/sec'
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    c = urllib.parse.quote_plus(json.dumps(chart))
+    return f"https://quickchart.io/chart?c={c}"
+
+
+def send_plots_to_slack(hook, results, files, channel):
+    def config_to_string(config):
+        return json.dumps(config)
+
+    data = {
+        'blocks': [
+            {
+                "type": "image",
+                "title": {
+                    "type": "plain_text",
+                    "text": r["name"],
+                },
+                "image_url": files[r['name']],
+                "alt_text": "Performance graph"
+            } for r in results]
+    }
+
+    requests.post(hook, json=data)
 
 
 def main():
     args = parse_arguments()
     results = load_performance_tests(getattr(args, 'server.endpoint'), getattr(args, 'server.user'),
                                      not getattr(args, 'server.no_verify_tls'), args.verbose)
+    print(results)
+    charts = {}
     for result in results:
-        plot(result['name'], result['latest_results'])
+        url = to_chart_js(result['name'], result['latest_results'])
+        print(url)
+        charts[result['name']] = url
+
+    send_plots_to_slack(getattr(args, 'slack.web_hook'), results, charts,
+                        getattr(args, 'slack.channel_id'))
 
 
 if __name__ == "__main__":
